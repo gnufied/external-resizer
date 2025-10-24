32a02f48efc5f2d04ab507823f306e986e2d6f18
Hemant Kumar <hekumar@redhat.com>
2025-10-24 15:38:31 -0400

    commiting uncommited changes on 2025-10-24 15:38:31 -0400

--- .prow.sh
+++ .prow.sh
@@ -12,7 +12,7 @@ export CSI_PROW_E2E_FOCUS_1_34="\[FeatureGate:VolumeAttributesClass\]"
 
 custom_post_install() {
     echo "!!!! Running custom post install hook... !!!!!!!!!!"
-    yq -i '.VolumeAttributesClass.FromExistingClassName = testvac' "${CSI_PROW_WORK}/test-driver.yaml"
+    yq -i '.VolumeAttributeClass = {"FromExistingClassName": "testvac"}' "${CSI_PROW_WORK}/test-driver.yaml"
     cat "${CSI_PROW_WORK}/test-driver.yaml"
 
     # Create the VAC Class
@@ -25,16 +25,16 @@ driverName: hostpath.csi.k8s.io
 parameters:
   e2eVacTest: e2eVacTestValue
 EOF
-    info "Creating sample VAC class"
+    info "!!!!!!!!!! Creating sample VAC class !!!!!!!!!! "
     cat "${CSI_PROW_WORK}/sample-vac.yaml"
     kubectl create -f "${CSI_PROW_WORK}/sample-vac.yaml"
-    info "Modifying CSI driver to enable VAC"
+    info "########## Modifying CSI driver to enable VAC ##########"
     kubectl get statefulsets csi-hostpathplugin  -o json|jq '(.spec.template.spec.containers[] | select(.name == "hostpath").args) += ["-enable-controller-modify-volume", "--accepted-mutable-parameter-names=e2eVacTest"]'|kubectl apply -f -
 
-    info "Rolling out statefulset"
+    info "!!!!!!!!!! Rolling out statefulset !!!!!!!!!!"
     kubectl rollout restart sts csi-hostpathplugin -n default
     # Wait for the rollout to complete
-    info "Waiting for rollout to complete..."
+    info "########## Waiting for rollout to complete..."
     kubectl rollout status sts csi-hostpathplugin -n default
 }
 
