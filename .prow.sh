32c759179e815a16d59f9eccc76c0dec7de0b35a
Hemant Kumar <hekumar@redhat.com>
2025-10-24 14:18:14 -0400

    commiting uncommited changes on 2025-10-24 14:18:14 -0400

--- .prow.sh
+++ .prow.sh
@@ -12,8 +12,25 @@ export CSI_PROW_E2E_FOCUS_1_34="\[FeatureGate:VolumeAttributesClass\]"
 
 custom_post_install() {
     echo "!!!! Running custom post install hook... !!!!!!!!!!"
-    yq -i '.VolumeAttributesClass.FromName = true' "${CSI_PROW_WORK}/test-driver.yaml"
+    yq -i '.VolumeAttributesClass.FromExistingClassName = testvac' "${CSI_PROW_WORK}/test-driver.yaml"
     cat "${CSI_PROW_WORK}/test-driver.yaml"
+
+    # Create the VAC Class
+    cat >"${CSI_PROW_WORK}/sample-vac.yaml" <<EOF
+apiVersion: storage.k8s.io/v1
+kind: VolumeAttributesClass
+metadata:
+  name: testvac
+driverName: hostpath.csi.k8s.io
+parameters:
+  e2eVacTest: e2eVacTestValue
+EOF
+    info "Creating sample VAC class"
+    cat "${CSI_PROW_WORK}/sample-vac.yaml"
+    kubectl create -f "${CSI_PROW_WORK}/sample-vac.yaml"
+
+    info "Dropping into the shell"
+    bash -i
 }
 
 export CSI_PROW_DRIVER_POSTINSTALL="custom_post_install"
