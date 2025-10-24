13e636895d4904c713828f8499d81eb88bb647e4
Hemant Kumar <hekumar@redhat.com>
2025-10-24 13:18:02 -0400

    commiting uncommited changes on 2025-10-24 13:18:02 -0400

--- .prow.sh
+++ .prow.sh
@@ -12,7 +12,7 @@ export CSI_PROW_E2E_FOCUS_1_34="\[FeatureGate:VolumeAttributesClass\]"
 
 custom_post_install() {
     echo "!!!! Running custom post install hook... !!!!!!!!!!"
-    yq -i '.VolumeAttributeClass.FromName = true' "${CSI_PROW_WORK}/test-driver.yaml"
+    yq -i '.VolumeAttributesClass.FromName = true' "${CSI_PROW_WORK}/test-driver.yaml"
     cat "${CSI_PROW_WORK}/test-driver.yaml"
 }
 
